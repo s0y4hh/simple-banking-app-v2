28a93fff884efa0dc86fdc48dd2d36b928171362
lanlanjr <allanibojr@cspc.edu.ph>
2025-04-20 00:27:51 +0800

    Update MySQL port retrieval in app.py to include a default value of '3306', ensuring compatibility with environment variable settings.

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def create_app():
         mysql_user = os.environ.get('MYSQL_USER')
         mysql_password = os.environ.get('MYSQL_PASSWORD')
         mysql_host = os.environ.get('MYSQL_HOST')
-        mysql_port = os.environ.get('MYSQL_PORT')
+        mysql_port = os.environ.get('MYSQL_PORT', '3306')
         mysql_database = os.environ.get('MYSQL_DATABASE')
         
         # Make sure all values are strings
